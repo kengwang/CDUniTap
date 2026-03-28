48cdec1164f3d4bb53b3ddaf0af9b73269644660
Kengwang <github@kengwang.com.cn>
2026-03-28 22:42:45 +0800

    chore: 修复代码合并错误

--- CDUniTap/Services/Api/CasServiceApi.cs
+++ CDUniTap/Services/Api/CasServiceApi.cs
@@ -16,9 +16,7 @@ public partial class CasServiceApi : IHttpApiServiceBase
     private readonly HttpClient _httpClient;
     private readonly CasServiceApiOptions _options;
 
-    private static readonly RSACryptoServiceProvider RsaProvider = CreateRsaProviderFromPublicKey(
-        "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyor3CX6A6U4EoSHawtALiJoB0CkJnb/wmVkcVT5EmNupGVrVSeJo80ZAxsgd9S1CZVXxTXtJ7XjsqnzR64Qvrn+tdvj9Ck5k/6Tnp6HoKU/AQxA3tQ5Zqw6D6ihPOyVV4z4cdK5wjzEBNPhJuTjjzP4VQ4h4VseWNbfhXGK3vSes8oNn5Wwor9r1UbEJP/ZMHrDJxAcwe0GPvebAqEp4O5ZcTtWnq+/qkoUB6z/52EnCMltoPmuMC+o3fWdICBf4q70oSDClfuhLVi4mRT2K5UUH8fsxEe6oPtkvk9vVCCOZRmo0MXpXZiIqdZOtgcBzn/0mzoNd58KxeIy0ginjfwIDAQAB");
-
+    
     public CasServiceApi(HttpClient httpClient, CasServiceApiOptions options)
     {
         _httpClient = httpClient;
